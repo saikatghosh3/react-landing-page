3abb3ad690325e8412c1c7712aa1e8539c0b531a
Saikat Ghosh <saikat.blast20@gmail.com>
2025-11-09 23:24:51 +0600

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Hero from './components/hero'
 import Introduce from './components/Introduce'
 import LightSection from './components/LightSection'
